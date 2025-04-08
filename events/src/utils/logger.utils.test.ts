059a245595a6972c2d3e3bf1a3873fee3165dd39
glo <glo.pena@dovetech.com>
2025-04-08 09:00:09 +0100

    remove commented out

--- events/src/utils/logger.utils.test.ts
+++ events/src/utils/logger.utils.test.ts
@@ -8,11 +8,4 @@ describe('getLogger', () => {
     expect(logger).toHaveProperty('info');
     expect(logger).toHaveProperty('error');
   });
-
-  //   it('should return the same logger instance on subsequent calls', () => {
-  //     const logger1 = getLogger();
-  //     const logger2 = getLogger();
-
-  //     expect(logger1).toBe(logger2);
-  //   });
 });
